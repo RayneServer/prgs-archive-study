fad97956c03166e4c7251624acbf86d4c05b2588
Rayne <pluvstream@gmail.com>
2026-05-10 23:18:18 +0900

    260510

---
+++ src/service/rayne/prgs/Level0/P181952.java
@@ -0,0 +1,11 @@
+package service.rayne.prgs.Level0;
+
+import java.util.*;
+
+public class P181952 {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.print(scanner.nextLine());
+  }
+}
