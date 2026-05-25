c1f352489a8ff3ace327ede027ad4daa28d5c9bc
Rayne <pluvstream@gmail.com>
2026-05-25 21:33:57 +0900

    260525

---
+++ src/service/rayne/prgs/Level0/P120829.java
@@ -0,0 +1,10 @@
+package service.rayne.prgs.Level0;
+
+public class P120829 {
+  public int solution(int angle) {
+    if (angle < 90) return 1;
+    else if (angle == 90) return 2;
+    else if (angle < 180) return 3;
+    else return 4;
+  }
+}
