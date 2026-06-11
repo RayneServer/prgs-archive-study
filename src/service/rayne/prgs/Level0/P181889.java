b2c6841592621d7f9fc5bb194c1d0277a5337af3
Rayne <pluvstream@gmail.com>
2026-06-11 23:43:32 +0900

    260611

---
+++ src/service/rayne/prgs/Level0/P181889.java
@@ -0,0 +1,9 @@
+package service.rayne.prgs.Level0;
+
+import java.util.Arrays;
+
+public class P181889 {
+  public int[] solution(int[] num_list, int n) {
+    return Arrays.copyOfRange(num_list, 0, n);
+  }
+}
