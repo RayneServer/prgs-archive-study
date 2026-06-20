e30c0779ffe9625d84387561801947272d572b76
Rayne <pluvstream@gmail.com>
2026-06-20 17:57:53 +0900

    260620

---
+++ src/service/rayne/prgs/Level0/P181839.java
@@ -0,0 +1,17 @@
+package service.rayne.prgs.Level0;
+
+public class P181839 {
+  public int solution(int a, int b) {
+    int result = 0;
+
+    if (a % 2 != 0 && b % 2 != 0) {
+      result = (a * a) + (b * b);
+    } else if (a % 2 == 0 && b % 2 == 0) {
+      result = Math.abs(a - b);
+    } else {
+      result = 2 * (a + b);
+    }
+
+    return result;
+  }
+}
