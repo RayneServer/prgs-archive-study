ef1581bec67957cfa1a8cafa44de495d7fc05108
Rayne <pluvstream@gmail.com>
2026-06-27 17:55:10 +0900

    260627

---
+++ src/service/rayne/prgs/Level0/P181920.java
@@ -0,0 +1,12 @@
+package service.rayne.prgs.Level0;
+
+public class P181920 {
+  public int[] solution(int start_num, int end_num) {
+    int[] answer = new int[end_num - start_num + 1];
+    for (int i = start_num; i <= end_num; i++) {
+      answer[i - start_num] = i;
+    }
+
+    return answer;
+  }
+}
