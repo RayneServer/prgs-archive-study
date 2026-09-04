d175380ad7fefb3f8b71e80d7669f15dcf450e24
RAYNE <pluvstream@gmail.com>
2026-09-04 12:57:28 +0900

    260904

---
+++ src/service/rayne/prgs/Level0/P120895.java
@@ -0,0 +1,12 @@
+package service.rayne.prgs.Level0;
+
+public class P120895 {
+  public String solution(String my_string, int num1, int num2) {
+    String[] strArr = my_string.split("");
+    String temp = strArr[num1];
+    strArr[num1] = strArr[num2];
+    strArr[num2] = temp;
+
+    return String.join("", strArr);
+  }
+}
