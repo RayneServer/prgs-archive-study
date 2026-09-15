2d2b897e848032f05ae6838c3a885aad6735cd7b
RAYNE <pluvstream@gmail.com>
2026-09-15 21:27:16 +0900

    260915

---
+++ src/service/rayne/prgs/Level0/P340202.java
@@ -0,0 +1,17 @@
+package service.rayne.prgs.Level0;
+
+public class P340202 {
+  public int solution(int storage, int usage, int[] change) {
+    int total_usage = 0;
+
+    for (int i = 0; i < change.length; i++) {
+      usage = usage + (usage * change[i] / 100);
+      total_usage += usage;
+      if (total_usage > storage) {
+        return i;
+      }
+    }
+
+    return -1;
+  }
+}
