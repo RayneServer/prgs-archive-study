833e21827207f10d9c40e6f1da267a3e213663dd
RAYNE <pluvstream@gmail.com>
2026-09-23 23:39:46 +0900

    260923

--- src/service/rayne/prgs/Level1/P140108.java
+++ src/service/rayne/prgs/Level1/P140108.java
@@ -1,4 +1,32 @@
 package service.rayne.prgs.Level1;
 
 public class P140108 {
+  public int solution(String s) {
+    int count = 0;
+
+    Master:
+    while (s.length() > 0) {
+      char word = s.charAt(0);
+      int wordCount = 0;
+      int notWordCount = 0;
+
+      for (int i = 0; i < s.length(); i++) {
+        char c = s.charAt(i);
+
+        if (word == c) wordCount++;
+        else notWordCount++;
+
+        if (wordCount == notWordCount) {
+          count++;
+          s = s.substring(i + 1);
+          continue Master;
+        }
+      }
+
+      count++;
+      s = "";
+    }
+
+    return count;
+  }
 }
